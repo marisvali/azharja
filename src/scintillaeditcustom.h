6f6e56cde08e4cec2232e8d603300c34e96a4b3e
Vali Maris <marisvali@yahoo.com>
2023-10-22 23:26:32 +0300

    fix compilation in debug mode using cmake

--- src/scintillaeditcustom.h
+++ src/scintillaeditcustom.h
@@ -1,19 +1,27 @@
 #ifndef SCINTILLAEDITCUSTOM_H
 #define SCINTILLAEDITCUSTOM_H
 
+// This #define took a lot of hours to uncover.
+// Without it, MSVC 2019 complains that there are unresolved external symbols
+// when linking to the debug version of the static lib. It has something to do
+// with __declspec(dllexport) and __declspec(dllimport). I don't know enough
+// about this aspect of Microsoft C++ coding to understand why adding this
+// define solves the problem. I suspect it has something to do with the
+// Scintilla component expecting to be used as a DLL and I'm using it as a
+// static library.
+#define MAKING_LIBRARY
 #include <ScintillaEdit.h>
 
-class ScintillaEditCustom : public ScintillaEdit
-{
-    Q_OBJECT
+class ScintillaEditCustom : public ScintillaEdit {
+  Q_OBJECT
 public:
-    explicit ScintillaEditCustom(QFont font, QWidget *parent = nullptr);
-    
+  explicit ScintillaEditCustom(QFont font, QWidget *parent = nullptr);
+
 signals:
-    
+
 private slots:
-    void CharAdded(int ch);
-    void AddDate();
+  void CharAdded(int ch);
+  void AddDate();
 };
 
 #endif // SCINTILLAEDITCUSTOM_H
