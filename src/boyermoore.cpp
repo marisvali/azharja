0886c08fa4f5fceb08710fb724db3b4936f084f4
dnsined <savandenis607@gmail.com>
2025-02-08 15:24:32 +0200

    adjusted for fitting the azharja style

--- src/boyermoore.cpp
+++ src/boyermoore.cpp
@@ -1,10 +1,10 @@
 #include "boyermoore.h"
 
-#include <iostream>
 
 constexpr size_t ALPHABET_LEN = 256;
 
-void CreateDelta1(QVector<ptrdiff_t>& delta1, const QString& pat) {
+void CreateDelta1(QVector<ptrdiff_t>& delta1, const QString& pat)
+{
     size_t patlen = pat.length();
     for (size_t i = 0; i < ALPHABET_LEN; i++)
         delta1[i] = patlen;
@@ -12,24 +12,25 @@ void CreateDelta1(QVector<ptrdiff_t>& delta1, const QString& pat) {
         delta1[pat[i].unicode()] = patlen - 1 - i;
 }
 
-bool IsPrefix(const QString& word, size_t wordlen, ptrdiff_t pos) {
+bool IsPrefix(const QString& word, size_t wordlen, ptrdiff_t pos)
+{
     size_t suffixlen = wordlen - pos;
     for (size_t i = 0; i < suffixlen; i++)
-    {
         if (word[i] != word[pos + i])
             return false;
-    }
     return true;
 }
 
-size_t SuffixLength(const QString& word, size_t wordlen, ptrdiff_t pos) {
+size_t SuffixLength(const QString& word, size_t wordlen, ptrdiff_t pos)
+{
     size_t i = 0;
     while ((pos >= i) && (word[pos - i] == word[wordlen - 1 - i]))
         i++;
     return i;
 }
 
-void CreateDelta2(QVector<ptrdiff_t>& delta2, const QString& pat) {
+void CreateDelta2(QVector<ptrdiff_t>& delta2, const QString& pat)
+{
     size_t patlen = pat.length();
     qint64 last_prefix_index = patlen - 1;
     for (qint64 p = patlen - 1; p >= 0; p--)
@@ -43,14 +44,13 @@ void CreateDelta2(QVector<ptrdiff_t>& delta2, const QString& pat) {
         size_t slen = SuffixLength(pat, patlen, p);
         //add boundary checks
         if (p >= slen && p - slen < patlen && patlen - 1 - slen < patlen)
-        {
             if (pat[p - slen] != pat[patlen - 1 - slen])
                 delta2[patlen - 1 - slen] = patlen - 1 - p + slen;
-        }
     }
 }
 
-QVector<size_t> SearchStringPattern(const QString& text, const QString& pattern) {
+QVector<size_t> SearchStringPattern(const QString& text, const QString& pattern)
+{
     QVector<size_t> occurrences;
     QVector<ptrdiff_t> delta1(ALPHABET_LEN);
     QVector<ptrdiff_t> delta2(pattern.length());
